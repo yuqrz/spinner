ea50da7461bb54268bf61553960d862a55d3bd61
yuqrz <pjesupomczek@gmail.com>
2025-06-04 15:37:24 +0200

    dodane dodatki

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -13,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner unitSpinner;
     Spinner unnitSpinner;
+    Spinner unitSpinnerr;
     TextView selectedUnitTextView;
     TextView selectedUnnitTextView;
+    TextView selectedUnitTextVieww;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
         unitSpinner = findViewById(R.id.unitSpinner);
         unnitSpinner = findViewById(R.id.unnitSpinner);
+        unitSpinnerr = findViewById(R.id.unitSpinnerr);
         selectedUnitTextView = findViewById(R.id.selectedUnitTextView);
         selectedUnnitTextView = findViewById(R.id.selectedUnnitTextView);
+        selectedUnitTextVieww = findViewById(R.id.selectedUnitTextVieww);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.length_units_array, android.R.layout.simple_spinner_item);
@@ -35,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
                 R.array.rodzaj_ciasta, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         unnitSpinner.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.dodatki_ma_pizze, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitSpinnerr.setAdapter(adapter);
 
         unitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -65,6 +74,17 @@ public class MainActivity extends AppCompatActivity {
                 selectedUnnitTextView.setText("Rodzaj: -");
             }
 
+        });
+        unitSpinnerr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedUnit = parent.getItemAtPosition(position).toString();
+                selectedUnitTextVieww.setText("Dodatki: " + selectedUnit);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selectedUnitTextVieww.setText("Dodatki: -");
+            }
         });
         }
     }
